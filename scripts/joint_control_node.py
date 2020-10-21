9db1e684511720dbb3ba44fe70f51db474922923
shuailiz <shuailirpi@gmail.com>
2020-10-21 01:08:21 -0700

    Add start log info in the joint controller

--- scripts/joint_control_node.py
+++ scripts/joint_control_node.py
@@ -5,5 +5,6 @@ from spotmicro_motor_control.joint_controller import SpotJointController
 
 if __name__ == '__main__':
     rospy.init_node('spot_joint_control_node')
+    rospy.loginfo("Joint controller for spot has been started")
     joint_control = SpotJointController()
     joint_control.run()
